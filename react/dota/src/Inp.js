711b15d75d55bf0f1383bb645994f6ed1d92b738
Рома  Белых <romanbelyh436@gmail.com>
2023-03-28 20:27:17 +0400

    init
    roma did it

---
+++ react/dota/src/Inp.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import "./App.css"
+export default function Inp() {
+const [change,forChange] = useState[""]
+ let filter = []
+ 
+  return (
+    <div>dds</div>
+   )
+
+  }
